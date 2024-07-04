ff49c093cbe2cee84204ec790d0000b4979e19e4
dmitrynovik <dmitry.novik@gmail.com>
2024-07-04 16:48:14 +1000

    feat: adding configuration for RabbitMQ

---
+++ src/main/java/com/example/rabbitmqCustomMetrics/RabbitMQConfig.java
@@ -0,0 +1,16 @@
+package com.example.rabbitmqCustomMetrics;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration(proxyBeanMethods = false)
+public class RabbitMQConfig {
+    // TODO: read from configuration
+    private String userName = "admin";
+    private String password = "admin"; 
+    private String host = "172.18.255.200";
+    private String schema = "http";
+    
+    public String getConnectionString() {
+        return schema + "://" + userName + ":" + password + "@" + host;
+    }
+}
--- src/main/java/com/example/rabbitmqCustomMetrics/RabbitMQController.java
+++ src/main/java/com/example/rabbitmqCustomMetrics/RabbitMQController.java
@@ -6,6 +6,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RabbitMQController {
 
+	private RabbitMQConfig rabbitMQConfig;
+
+	public RabbitMQController(RabbitMQConfig rabbitMQConfig) {
+		this.rabbitMQConfig = rabbitMQConfig;
+
+	}
+
 	@GetMapping("/queue-size-limits")
 	public String index() {
 		return "Greetings from Spring Boot!";
