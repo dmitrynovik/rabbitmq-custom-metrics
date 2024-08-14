1d215ffda096fa94de6971097e1d3c4e9cb756a4
dmitrynovik <dmitry.novik@gmail.com>
2024-08-14 17:25:51 +1000

    chore: refactor

--- src/main/java/com/example/rabbitmqCustomMetrics/services/RabbitMQService.java
+++ src/main/java/com/example/rabbitmqCustomMetrics/services/RabbitMQService.java
@@ -26,20 +26,22 @@ public class RabbitMQService {
             .toArray(MaxLenPolicyUtilisation[]::new);
     }
 
-    private static MaxLenPolicyUtilisation convert(QueueResponse queue) {
+    public static MaxLenPolicyUtilisation convert(QueueResponse queue) {
         Effective_policy_definition policy = queue.getEffective_policy_definition();
         float utilisation = 0;
 
         if (policy != null) {
+            // check utilisation as no. of messages:
             long maxLength = policy.getMaxLength();
             if (maxLength > 0) {
                 utilisation = queue.getMessages() / maxLength;
             }
-
+            
+            // check utlisation as no. of bytes:
             long maxLengthBytes = policy.getMaxLengthBytes();
             if (maxLengthBytes > 0) {
-                float utilisationMax = queue.getMessage_bytes() / maxLengthBytes;
-                utilisation = Math.max(utilisation, utilisationMax);
+                float utilisationBytes = queue.getMessage_bytes() / maxLengthBytes;
+                utilisation = Math.max(utilisation, utilisationBytes);
             }
         }
 
