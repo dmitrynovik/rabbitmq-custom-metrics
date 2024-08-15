d70f916f8b2a60c2023823d3df394303b4179d26
dmitrynovik <dmitry.novik@gmail.com>
2024-08-15 15:09:00 +1000

    feat: MetricsService.java

--- src/main/java/com/example/rabbitmqCustomMetrics/RabbitMQController.java
+++ src/main/java/com/example/rabbitmqCustomMetrics/RabbitMQController.java
@@ -1,67 +1,23 @@
 package com.example.rabbitmqCustomMetrics;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
-import com.example.rabbitmqCustomMetrics.config.LokiConfig;
-import com.example.rabbitmqCustomMetrics.config.RabbitMQConfig;
 import com.example.rabbitmqCustomMetrics.models.rabbitmq.MaxLenPolicyUtilisation;
-import com.example.rabbitmqCustomMetrics.services.LokiService;
-import com.example.rabbitmqCustomMetrics.services.RabbitMQService;
-
+import com.example.rabbitmqCustomMetrics.services.MetricsService;
 import java.io.IOException;
-import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.http.*;
-import java.net.http.HttpResponse.BodyHandlers;
-import java.util.Base64;
 
 @RestController
 public class RabbitMQController {
 
-	private final RabbitMQConfig rabbitMQConfig;
-	private static final Logger log = LoggerFactory.getLogger(RabbitMQController.class);
-	private HttpClient httpClient;
-	private RabbitMQService rabbitMQService;
-	private LokiService lokiService;
+	private final MetricsService metricsService;
 
-	public RabbitMQController(RabbitMQConfig rabbitMQConfig, LokiConfig lokiConfig, RestTemplate restTemplate, 
-		RabbitMQService rabbitMQService, LokiService lokiService) {	
-		this.rabbitMQConfig = rabbitMQConfig;
-		this.rabbitMQService = rabbitMQService;
-		this.lokiService = lokiService;
-		this.httpClient = HttpClient.newHttpClient();
+	public RabbitMQController(MetricsService metricsService) {	
+		this.metricsService = metricsService;
 	}
 
 	@GetMapping("/rabbitmq/queue/utilization")
 	public MaxLenPolicyUtilisation[] index() throws URISyntaxException, IOException, InterruptedException {
-		final String uri = rabbitMQConfig.getConnectionString() + "/api/queues";
-		log.info("HTTP GET " + uri);
-
-		HttpRequest request = createHttpRequest(uri);
-
-		HttpResponse<String> response = httpClient.send(request, BodyHandlers.ofString());
-		String payload = response.body();
-		//log.info(payload);
-		MaxLenPolicyUtilisation[] utilisations = rabbitMQService.getQueueMaxLengthUtilization(payload);
-		lokiService.logQueueUtilisationLogFmt(utilisations);
-		return utilisations;
+		return this.metricsService.getQueueUtilizationMetrics();
 	}
-
-	private HttpRequest createHttpRequest(final String uri) throws URISyntaxException {
-		return HttpRequest.newBuilder()
-			.GET()
-			.uri(new URI(uri))
-			.header("Authorization", getBasicAuthenticationHeader())
-			.build();
-	}
-
-	private final String getBasicAuthenticationHeader() {
-		String valueToEncode = rabbitMQConfig.getUserName() + ":" + rabbitMQConfig.getPassword();
-		return "Basic " + Base64.getEncoder().encodeToString(valueToEncode.getBytes());
-	}
-
 }
---
+++ src/main/java/com/example/rabbitmqCustomMetrics/services/MetricsService.java
@@ -0,0 +1,62 @@
+package com.example.rabbitmqCustomMetrics.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.example.rabbitmqCustomMetrics.RabbitMQController;
+import com.example.rabbitmqCustomMetrics.config.LokiConfig;
+import com.example.rabbitmqCustomMetrics.config.RabbitMQConfig;
+import com.example.rabbitmqCustomMetrics.models.rabbitmq.MaxLenPolicyUtilisation;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.*;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.Base64;
+
+@Service
+public class MetricsService {
+    private final RabbitMQConfig rabbitMQConfig;
+	private static final Logger log = LoggerFactory.getLogger(RabbitMQController.class);
+	private HttpClient httpClient;
+	private RabbitMQService rabbitMQService;
+	private LokiService lokiService;
+
+	public MetricsService(RabbitMQConfig rabbitMQConfig, LokiConfig lokiConfig, RestTemplate restTemplate, 
+		RabbitMQService rabbitMQService, LokiService lokiService) {	
+		this.rabbitMQConfig = rabbitMQConfig;
+		this.rabbitMQService = rabbitMQService;
+		this.lokiService = lokiService;
+		this.httpClient = HttpClient.newHttpClient();
+	}
+
+	public MaxLenPolicyUtilisation[] getQueueUtilizationMetrics() throws URISyntaxException, IOException, InterruptedException {
+		final String uri = rabbitMQConfig.getConnectionString() + "/api/queues";
+		log.info("HTTP GET " + uri);
+
+		HttpRequest request = createHttpRequest(uri);
+
+		HttpResponse<String> response = httpClient.send(request, BodyHandlers.ofString());
+		String payload = response.body();
+		//log.info(payload);
+		MaxLenPolicyUtilisation[] utilisations = rabbitMQService.getQueueMaxLengthUtilization(payload);
+		lokiService.logQueueUtilisationLogFmt(utilisations);
+		return utilisations;
+	}
+
+	private HttpRequest createHttpRequest(final String uri) throws URISyntaxException {
+		return HttpRequest.newBuilder()
+			.GET()
+			.uri(new URI(uri))
+			.header("Authorization", getBasicAuthenticationHeader())
+			.build();
+	}
+
+	private final String getBasicAuthenticationHeader() {
+		String valueToEncode = rabbitMQConfig.getUserName() + ":" + rabbitMQConfig.getPassword();
+		return "Basic " + Base64.getEncoder().encodeToString(valueToEncode.getBytes());
+	}
+}
